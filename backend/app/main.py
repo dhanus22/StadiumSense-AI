ca6fa4e75f7dfc1c8836725287ab6f6e77c2fe16
dhanus22 <smsaidhanush153@gmail.com>
2026-07-17 16:37:01 +0530

    cors filter alter

--- backend/app/main.py
+++ backend/app/main.py
@@ -52,6 +52,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://stadium-sense-ai-two.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
